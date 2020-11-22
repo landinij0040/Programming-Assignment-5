2414640ac0aa5660cbd28e5c6cc0be2f3543da54
Yulianna Torres <yuliannatorres109@gmail.com>
2020-11-21 19:33:34 -0700

    Savings, Checking, Credit increment by 1 in max

--- src/BankManager.java
+++ src/BankManager.java
@@ -1,7 +1,4 @@
 /**
- * @author Yulianna Torres, Isaiah Landin
- * @since 11/13/2020
- *
  * Class Description:
  * BankManager has all bank manager functionality.
  *
@@ -10,6 +7,7 @@
  * 2) Bank manager generates bank statements
  * 3) there is only one bank manager for the whole bank
  * 4) There will never be an account number = 0
+ *
  * @author Yulianna Torres, Isaiah Landin
  * @since 11/13/2020
  */
